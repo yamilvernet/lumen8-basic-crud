6d1bb1e228eb6794ad75f2042ec3686477e20b04
Yamil Vernet <yamil.vernet@gmail.com>
2023-09-21 01:17:57 -0300

    add:provider for Lumen Generator

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -94,6 +94,8 @@ $app->routeMiddleware([
 $app->register(App\Providers\AuthServiceProvider::class);
 // $app->register(App\Providers\EventServiceProvider::class);
 $app->register(Tymon\JWTAuth\Providers\LumenServiceProvider::class);
+$app->register(Flipbox\LumenGenerator\LumenGeneratorServiceProvider::class);
+
 /*
 |--------------------------------------------------------------------------
 | Load The Application Routes
